5a102813dc263996248f5401fba5a9a398f3e771
omb <baldeoumar093@gmail.com>
2024-05-13 15:06:03 +0000

    first commit

--- PizzaMama/PizzaMama/settings.py
+++ PizzaMama/PizzaMama/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(p%1e@)=$(@$58fi+=09@$r&rd&q3wd1zaivw37ko&r73g*!$@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["oumarbalde.pythonanywhere.com"]
 
 
 # Application definition
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'menu.apps.MenuConfig'
+    'menu.apps.MenuConfig',
+    'main.apps.MainConfig'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
--- PizzaMama/menu/views.py
+++ PizzaMama/menu/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pizza
 # Create your views here.
 
 def index(request):
-    return HttpResponse('les pizzas')
+    pizza = Pizza.objects.all()
+    """pizza_details = []
+    for pizza in pizza:
+        pizza_details.append(pizza.nom+" : "+str(pizza.print)+" xof")
+    return HttpResponse("les pizzas: "+", ".join(pizza_details))"""
+    return render(request,'menu/index.html',{'pizza':pizza})
